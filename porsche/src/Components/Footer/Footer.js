7aebe791891d7348f26ad8a96daa0abeeab2986c
truefuego <ankitsharma15092003@gmail.com>
2023-12-01 01:00:52 +0530

    ---

--- porsche/src/Components/Footer/Footer.js
+++ porsche/src/Components/Footer/Footer.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './Footer.css'
 import ShareIcon from '../../Assets/Images/Icons/share.png'
 import FaceBook from '../../Assets/Images/Icons/facebook.png'
@@ -9,7 +9,50 @@ import LinkedIn from '../../Assets/Images/Icons/linkedin.png'
 import Twitter from '../../Assets/Images/Icons/twitter.png'
 import Digg from '../../Assets/Images/Icons/digg.png'
 
+const Locations = [
+  {
+    region: "North America",
+    countries: ["Canada (English)", "Canada (French)", "USA"]
+  },
+  {
+    region: "Latin America",
+    countries: ["Mexico", "Argentina", "Bolivia", "Brazil", "Chile", "Colombia", "Dominican Republic", "Ecuador", "El Salvador", "Guadeloupe", "Guatemala", "Haiti", "Jamaica", "Panama", "Paraguay", "Peru", "Puerto Rico", "Trinidad & Tobago", "Uruguay", "Other countries"]
+  },
+  {
+    region: "Europe",
+    countries: ["Austria", "Belarus", "Belgium", "Bosnia and Herzegovina", "Bulgaria", "Croatia", "Cyprus", "Czech Republic", "Denmark", "Estonia", "Finland", "France", "Georgia", "Germany", "Great Britain", "Greece", "Hungary", "Iceland", "Ireland", "Italy", "Lithuania", "Latvia", "Luxemburg", "Macedonia", "Malta", "Moldova", "Netherlands", "Norway", "Poland", "Portugal", "Romania", "Serbia", "Slovakia", "Slovenia", "Spain", "Sweden", "Switzerland (German)", "Switzerland (French)", "Switzerland (Italian)", "Ukraine", "Other countries"]
+  },
+  {
+    region: "Middle East",
+    countries: ["Abu Dhabi", "Bahrin", "Dubai", "Fujairah", "Israel", "Jordan", "Kuwait", "Lebanon", "Oman", "Qatar", "Saudi Arabia", "Sharjah", "Middle East (English)", "Middle East (French)"]
+  },
+  {
+    region: "Africa",
+    countries: ["Egypt", "Ghana", "La Reunion", "Morocco", "Mauritius", "South Africa", "Tunisia"]
+  },
+  {
+    region: "Asia",
+    countries: ["Armenia", "Azerbaijan", "Turkey", "India", "Brunei", "Indonesia", "Malaysia", "Mongolia", "Philippines", "Singapore", "Sri Lanka", "Thailand", "Vietnam", "China Mainland (Chinese)", "China Mainland (English)", "Hong-Kong & Macau", "Japan", "Kazakhstan", "South Korea", "Taiwan (Chinese)" ,"Taiwan (English)" ,"Other countries/regions"]
+  },
+  {
+    region: "Australia/Oceania",
+    countries: ["Australia", "French Polynesia", "New Caledonia", "New Zealand", "Other countries"]
+  }
+]
+
 const Footer = () => {
+  const [region,setRegion] = useState('Select a region');
+  const [country,setCountry] = useState('Select a country/region');
+  const [countries,setCountries] = useState([])
+
+  const handleRegionSelect = (e) => {
+    setRegion(e.target.value)
+    setCountries(Locations.find(ctr => ctr.region === e.target.value).countries)
+  }
+
+  const handleCountrySelect = (e) => {
+    setCountry(e.target.value)
+  }
   return (
     <div className='footer'>
       <div className='container-center'>
@@ -92,7 +135,16 @@ const Footer = () => {
       </div>
       <div className='footer-lower'>
         <p className='footer-lower-header'>Change country/region:</p>
-
+        <div className='region-selector-area'>
+          <select value={region} onChange={handleRegionSelect} className='region-selector'> 
+            <option value={'Select a region'} selected disabled hidden>{'Select a region'}</option>
+            {Locations.map((item) => <option className='region-selector-option' value={item.region}>{item.region}</option>)}
+          </select> 
+            <select value={country} onChange={handleCountrySelect} className='region-selector'>
+              <option value={'Select a country/region'} selected disabled hidden>{'Select a country/region'}</option>
+              {countries !== undefined && countries.map((item) => <option className='region-selector-option' value={item}>{item}</option>)}
+            </select>
+        </div>
         <div className='footer-horizontal-line-lower'/>
         <div className='footer-last-line-area'>
           <p className='footer-last-line-text'>© 2023 Porsche India - SKODA AUTO Volkswagen India Private Limited - India.<u className='footer-last-line-underline'>Legal notice.</u><u className='footer-last-line-underline'>Cookie & Privacy Policy.</u><u className='footer-last-line-underline'>Corporate Governance.</u><u className='footer-last-line-underline'>Open Source Software Notice.</u><u className='footer-last-line-underline'>Whistleblower System.</u><u className='footer-last-line-underline'>Business and Human Rights.</u></p>
