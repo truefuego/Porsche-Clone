161d703082e66d3dfe7ba143a91363ae702b9175
truefuego <ankitsharma15092003@gmail.com>
2023-12-05 01:40:49 +0530

    ---

--- porsche/src/Pages/ModelsPage/ModelsPage.js
+++ porsche/src/Pages/ModelsPage/ModelsPage.js
@@ -1,9 +1,36 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './ModelsPage.css'
 import Navbar from '../../Components/Navbar/Navbar'
 import Footer from '../../Components/Footer/Footer'
+import ButtonColored from '../../Components/ButtonColored/ButtonColored'
+import Button_Image from '../../Assets/Images/Buttons/link-arrow-white-left.png'
+import Add_Image from '../../Assets/Images/Buttons/add.png'
+import Subtract_Image from '../../Assets/Images/Buttons/subtract.png'
+import { Cars } from '../../Stores/Cars'
 
 const ModelsPage = () => {
+  const [selectedFilter,setSelectedFilter] = useState({
+    models: '',
+    body_design: '',
+    transmission: '',
+    seats: '',
+    drive: '',
+    fuel_type: '',
+    price: '',
+    horsepower: ''
+  })
+
+  const [filtersToBeShown,setFiltersToBeShown] = useState({
+    models: '',
+    body_design: '',
+    transmission: '',
+    seats: '',
+    drive: '',
+    fuel_type: '',
+    price: '',
+    horsepower: ''
+  })
+
   return (
     <div>
       <Navbar />
@@ -13,8 +40,19 @@ const ModelsPage = () => {
         </div>
       </div>
       <div className='container-center'>
-        <div style={{width:'86%'}}>
-          
+        <div style={{width:'86%',marginTop:'32px',marginBottom:'32px'}}>
+          <ButtonColored Button_Color={'#313639'} Button_Image={Button_Image} Button_Text={'Back'} Button_Link={'#'}/>
+        </div>
+      </div>
+      <div className='container-center'>
+        <div className='model-page-porsche-car-configurator'>
+          <div className='porsche-car-configurator-filter-buttons'>
+
+          </div>
+          <div className='porsche-car-configurator-vertical-divider'/>
+          <div className='porsche-car-configurator-models'>
+
+          </div>
         </div>
       </div>
       <Footer />
